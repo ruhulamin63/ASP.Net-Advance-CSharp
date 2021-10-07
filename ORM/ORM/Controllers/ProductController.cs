4e2fb047aab8b06b7d7324698f1ea48f35ac5046
Ruhul Amin <raridoy4@gmail.com>
2021-10-07 22:36:00 +0600

    create ORM

---
+++ ORM/ORM/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using ORM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ORM.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+
+            var db = new ProductEntities1();
+
+            var data = db.products.ToList();
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(product s)
+        {
+            var db = new ProductEntities1();
+            db.products.Add(s);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+    }
+}
