a93ea6af6b0f5e51c1fac14cf57d3e920677a8d9
Ruhul Amin <raridoy4@gmail.com>
2021-10-17 17:04:31 +0600

    build e-commerce site

---
+++ E-Commerce/E-Commerce/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using E_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var db = new ProductEntities();
+
+            List<customer> data = db.customers.ToList();
+            return View(data);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var db = new ProductEntities();
+            var data = (from d in db.customers
+                        where d.Id == id
+                        select d).FirstOrDefault();
+
+            return View(data);
+        }
+    }
+}
---
+++ E-Commerce/E-Commerce/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using E_Commerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ORM.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var db = new ProductEntities();
+
+            List<product> data = db.products.ToList();
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(product s)
+        {
+            var db = new ProductEntities();
+            db.products.Add(s);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var db = new ProductEntities();
+            var product = (from p in db.products
+                           where p.Id == id
+                           select p).FirstOrDefault();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(product pro)
+        {
+            var db = new ProductEntities();
+
+            /* product.Name = pro.Name*/
+
+            var product = (from p in db.products
+                           where p.Id == pro.Id
+                           select p).FirstOrDefault();
+            db.Entry(product).CurrentValues.SetValues(pro);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(product pro)
+        {
+            var db = new ProductEntities();
+            var product = (from p in db.products
+                           where p.Id == pro.Id
+                           select p).FirstOrDefault();
+            db.products.Remove(product);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
