2ad00c83f272ba4ec9fbce3421ccc54e55e53e7a
Ruhul Amin <raridoy4@gmail.com>
2021-10-26 11:04:22 +0600

    create project file

---
+++ Home_service_asp_net_mvc_project/Home_service_asp_net_mvc_project/Controllers/ManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Home_service_asp_net_mvc_project.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Home_service_asp_net_mvc_project/Home_service_asp_net_mvc_project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Home_service_asp_net_mvc_project.MvcApplication" Language="C#" %>
