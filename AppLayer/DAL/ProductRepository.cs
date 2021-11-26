8f677d47389d2eef6f27082de92ff05c5a7287ff
Ruhul Amin <raridoy4@gmail.com>
2021-11-26 23:42:53 +0600

    delete for productRepo

--- AppLayer/DAL/ProductRepository.cs
+++ AppLayer/DAL/ProductRepository.cs
@@ -8,37 +8,6 @@ namespace DAL
 {
     public class ProductRepository : IRepository<product, int>
     {
-       /* static ProductEntities db;
-
-        static ProductRepository()
-        {
-            db = new ProductEntities();
-        }
-
-        public static List<product> GetAll()
-        {
-            return db.products.ToList();
-        }
-
-        public static product Get(int id)
-        {
-            return db.products.FirstOrDefault(e => e.Id == id);
-        }
-
-        public static void Edit(product p)
-        {
-            var pro = db.products.FirstOrDefault(e => e.Id == p.Id);
-            db.Entry(pro).CurrentValues.SetValues(pro);
-            db.SaveChanges();
-        }
-
-        public static void Delete(int id)
-        {
-            var pro = db.products.FirstOrDefault(e => e.Id == id);
-            db.products.Remove(pro);
-            db.SaveChanges();
-        }*/
-
         ProductEntities db;
 
         public ProductRepository(ProductEntities db)
